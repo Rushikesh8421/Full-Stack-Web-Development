7119b9bf14f4acbae489b2d859c0d4dd49194807
Rushikesh8421 <patilrushikesh8421@gmail.com>
2023-03-04 20:11:34 +0530

    Created Weather app with the help help of API OpneWeatherMap

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+
+var mySuperheroeName = superheroes.random();
+
+console.log(mySuperheroeName);
+var superVillains = require("supervillains");
+var mySuperVillainName = superVillains.random();
+console.log(mySuperVillainName);

d8a98f7452fb60f278c8669994d7e7dc3c2dba3c
Rushikesh8421 <patilrushikesh8421@gmail.com>
2023-06-02 12:28:18 +0530

    Connected with api

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,25 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
+
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/signup.html");
 });
 
 app.post("/",function(req,res){
-
-  var fname = req.body.fname;
-  var lname = req.body.lname;
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
   var email = req.body.email;
 
-  console.log(fname+" "+lname+" "+email);
+  console.log(firstName,lastName,email);
 
 });
 
 app.listen(3000,function(){
-  console.log("Server is running at port 3000");
+  console.log("server is running on post 3000");
 });
